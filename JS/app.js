dc1ff040404686f10a2dec7854e5a2f25337e641
FuadHashimov91 <fuadbh@code.edu.az>
2021-05-10 03:53:00 +0400

    Final

---
+++ JS/app.js
@@ -0,0 +1,64 @@
+import {xhr} from "./connection.js";
+import {CreateHTML} from "./createHtml.js";
+
+
+document.querySelector("#getAll").addEventListener("click",getAll);
+ 
+
+document.querySelector("#search").addEventListener("click",getUser);
+let table = document.getElementById("result-data");
+let errorMsg = document.getElementById("errorMsg");
+
+async function getUser(){
+    let input_val = document.getElementById("search-input").value;
+    let html ="";
+      await fetch(xhr.url)
+        .then(response => {
+          return response.json();
+        })
+        .then(data =>{
+          let finder =data.find(f => f.name == input_val || f.surname == input_val);    
+          return {
+            img:finder.img,
+            name:finder.name,
+            surname:finder.surname,
+            salary:finder.salary.value,
+            loan:finder.loans.map(m => m.loaner)
+          }
+        })
+        .then(data_finder =>{
+          return table.innerHTML = 
+            CreateHTML.addOne(data_finder);    
+        })
+        .catch(_=>{
+          table.innerHTML = "";
+          let e = new Error("Not Found");
+          return errorMsg.innerHTML =e;
+        })
+}
+
+
+async function getAll(){
+
+
+    await fetch(xhr.url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            let d = data.find(f =>f.loans.)
+
+            console.log(d);
+        })
+         
+        
+
+    
+}
+
+
+
+
+
+
+
---
+++ JS/connection.js
@@ -0,0 +1,4 @@
+export {xhr}
+let xhr = {
+    url:"https://api.npoint.io/ec21414b0e15972dbfde/data"
+}
